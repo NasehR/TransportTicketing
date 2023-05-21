aec6968bf56c42a78afcb28878b5dcb179960c40
Naseh <75029758+NasehR@users.noreply.github.com>
2023-05-21 23:52:19 +0800

    A state pattern for the status of a passenger i.e whether they are on travelling or not.

---
+++ src/Model/IPassengerStatus.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace TransportTicketing.Model
+{
+    public interface IPassengerStatus
+    {
+        string Status();
+        void On();
+        void Off();
+    }
+
+    public class OnTransport : IPassengerStatus
+    {
+        protected Passenger _passenger;
+
+        public OnTransport(Passenger passenger)
+        {
+            _passenger = passenger;
+        }
+
+        public string Status()
+        {
+            return "On Transport";
+        }
+
+        public void On()
+        {
+            Console.WriteLine("Passenger is already on transport");
+        }
+
+        public void Off() 
+        { 
+            Console.WriteLine("Getting off");
+            _passenger.SetStatus(new OffTransport(_passenger));
+        }
+    }
+
+    public class OffTransport : IPassengerStatus
+    {
+        protected Passenger _passenger;
+
+        public string Status()
+        {
+            return "Off Transport";
+        }
+
+        public OffTransport(Passenger passenger)
+        {
+            _passenger = passenger;
+        }
+
+        public void On()
+        {
+            _passenger.SetStatus(new OnTransport(_passenger));
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("Passenger is already off transport");
+        }
+    }
+}
