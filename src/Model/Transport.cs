cfaf217c45ec0e2d23ca525eb5c5f4d0b4d07e9e
Naseh Rizvi <75029758+NasehR@users.noreply.github.com>
2023-05-22 18:07:47 +0800

    Transport template. Started on the factory pattern.

---
+++ src/Model/Transport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TransportTicketing.Model
+{
+    public abstract class Transport
+    {
+        protected int NumberOfStations { get; }
+
+        public abstract void Move();
+    }
+
+    public class Bus : Transport
+    {
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Train : Transport
+    {
+        public override void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
