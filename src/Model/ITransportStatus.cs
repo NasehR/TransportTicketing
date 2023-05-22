ef424d3d52e4b2228a7ef69b7b29344bf2b2ba3e
Naseh <75029758+NasehR@users.noreply.github.com>
2023-05-23 07:05:35 +0800

    State pattern to toggle between "on-time", "delayed" and "cancelled" state of the transport.

---
+++ src/Model/ITransportStatus.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace TransportTicketing.Model
+{
+    public interface ITransportStatus
+    {
+        string ToString();
+        void Delayed();
+        void OnTime();
+        void Cancelled();
+    }
+
+    public class OnTimeState : ITransportStatus
+    {
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delayed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cancelled()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DelayedState : ITransportStatus
+    {
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delayed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnTime()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Cancelled()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CancelledState : ITransportStatus
+    {
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delayed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnTime()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void Cancelled()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
