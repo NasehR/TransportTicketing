c2e03446b9e2c8bc5db10cef44eb48df4c5cc1c8
Naseh <75029758+NasehR@users.noreply.github.com>
2023-05-23 01:22:39 +0800

    Transport Factory to uniquely create a transportation mode. Transport Client to interact with diferent transportation modes. Added methods to get number of passenger,number of stations, add passengers and  remove passengers.

---
+++ src/Controller/TransportClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TransportTicketing.Model;
+
+namespace TransportTicketing.Controller
+{
+    public class TransportClient
+    {
+        private readonly Transport _transport;
+
+        public TransportClient(TransportFactory transport, string transportationMode)
+        {
+            _transport = transport.CreateTransportation(transportationMode);
+        }
+
+        public int GetNumberOfStations()
+        {
+            return _transport.GetNumberOfStations();
+        }
+
+        public int GetNumberOfPassengers()
+        {
+            return _transport.GetNumberOfPassengers();
+        }
+        /*
+         * Passenger Id: arguement
+        public void AddPassenger()
+        {
+            _transport.AddPassengers();
+        }
+        */
+
+        /*
+         * Passenger Id: arguement
+        public void RemovePassenger()
+        {
+            _transport.RemovePassenger();
+        }
+        */
+    }
+}
--- src/Model/Transport.cs
+++ src/Model/Transport.cs
@@ -1,27 +1,111 @@
 ﻿using System;
+using static System.Collections.Specialized.BitVector32;
 
 namespace TransportTicketing.Model
 {
     public abstract class Transport
     {
-        protected int NumberOfStations { get; }
+        // Data structure to store the number of stations on the route
+        //protected Dictionary<String, Station> Stations { get; }
+        protected LinkedList<Passenger> Passengers { get; }
+        //protected Dictionary<String, Station> Stations { get; }
+        public abstract int GetNumberOfStations();
+        public abstract int GetNumberOfPassengers();
 
-        public abstract void Move();
+        //public abstract Station CurrentLocation();
+        //Passenger Id: arguement
+        //public abstract void AddPassengers();
+        //Passenger Id: arguement
+        //public abstract void RemovePassenger();
     }
 
     public class Bus : Transport
     {
-        public override void Move()
+        public Bus()
+        {
+            //Stations = new Dictionary<String, Station>();
+            //Passengers = new LinkedList<Passenger>();
+        }
+
+        public override int GetNumberOfStations()
+        {
+            if (Stations == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return Stations.Count;
+        }
+
+        public override int GetNumberOfPassengers()
+        {
+            if (Passengers == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return Passengers.Count();
+        }
+
+        /*
+        * Passenger Id: arguement
+        public override void AddPassengers()
         {
             throw new NotImplementedException();
         }
+        */
+
+        /*
+         * Passenger Id: arguement
+        public override void RemovePassenger()
+        {
+            throw new NotImplementedException();
+        }
+        */
     }
 
     public class Train : Transport
     {
-        public override void Move()
+        public Train()
+        {
+            //Stations = new Dictionary<String, Station>();
+            //Passengers = new LinkedList<Passenger>();
+        }
+
+        public override int GetNumberOfStations()
+        {
+            if (Stations == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return Stations.Count();
+        }
+
+        public override int GetNumberOfPassengers()
+        {
+            if (Passengers == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return Passengers.Count();
+        }
+
+        /*
+         * Passenger Id: arguement
+        public override void AddPassengers()
         {
             throw new NotImplementedException();
         }
+        */
+
+        /*
+         * Passenger Id: arguement
+        public override void RemovePassenger()
+        {
+            throw new NotImplementedException();
+        }
+        */
     }
 }
