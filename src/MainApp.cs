96c6b83ecfa484ac3f0974d135d8803ca8bad66f
Naseh <75029758+NasehR@users.noreply.github.com>
2023-05-29 01:29:28 +0800

    Successfully read the station and transport csv file.

--- src/MainApp.cs
+++ src/MainApp.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using TransportTicketing.Model;
 using TransportTicketing.Controller;
 using TransportTicketing.View;
+using Microsoft.VisualBasic.FileIO;
 
 namespace TransportTicketing
 {
     public static class MainApp
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
             try
             {
                 Console.WriteLine("MainApp");
                 Dictionary<String, PassengerController> Passengers = new Dictionary<String, PassengerController>();
                 Dictionary<String, Ticket> Tickets = new Dictionary<String, Ticket>();
-                Dictionary<String, Transport> Transports = new Dictionary<String, Transport>();
+                Dictionary<String, TransportClient> Transports = new Dictionary<String, TransportClient>();
+                List<Station> Stations = new List<Station>();
 
                 const string id = "SR070148614";
                 const string name = "Naseh Rizvi";
@@ -23,30 +25,41 @@ namespace TransportTicketing
                 const string dob = "06/05/2002";
                 PassengerController passenger = new PassengerController(id, name, billerCode, dob);
                 Passengers.Add(id, passenger);
+                Console.WriteLine("Passenger Added to Dictionary\n");
 
-                Console.WriteLine("Passenger Added to Dictionary");
+                string stationFileName = Path.GetFullPath(args[0]);
+                string transportFileName = Path.GetFullPath(args[1]);
 
-                string trainNumber = "999";
-                Transport bus = new Train();
-                Transports.Add(trainNumber, bus);
+                StationFileReader sFR = new StationFileReader(stationFileName);
+                Stations = sFR.ReadStationsFromCSV();
 
-                Console.WriteLine(passenger.GetCurrentStatus());
+                TransportFileReader tFR = new TransportFileReader(transportFileName, Stations);
+                Transports = tFR.ReadTransportsFromCSV();
 
-                passenger.Boarding(1, bus);
-                Console.WriteLine(passenger.GetCurrentStatus());
+                Console.Write("Passenger Status: ");
+                Console.WriteLine(Passengers["SR070148614"].GetCurrentStatus() + "\n");
 
-                passenger.Leaving();
-                Console.WriteLine(passenger.GetCurrentStatus());
+                Console.WriteLine("Passenger is boarding bus 999");
+                Passengers["SR070148614"].Boarding(1, Transports["999"]);
+                Console.WriteLine("Passenger is on bus 999");
+                Console.WriteLine(Passengers["SR070148614"].GetCurrentStatus() + "\n");
 
-                //int num = train.GetNumberOfPassengers();
+                Console.WriteLine("Passenger is leaving bus 999");
+                Passengers["SR070148614"].Leaving(Transports["999"]);
+                Console.WriteLine(Passengers["SR070148614"].GetCurrentStatus());
 
-                //Console.WriteLine(num);
+                Passengers["SR070148614"].Boarding(2, Transports["999"]);
+                Console.WriteLine("Passenger is off bus 999");
+                Console.WriteLine(Passengers["SR070148614"].GetCurrentStatus() + "\n");
 
-                //foreach (var p in train.GetCurrentPassengers())
-                //{
-                //    Console.WriteLine("Begin for each loop");
-                //    p.PrintPassengerDetails();
-                //}
+                int num = Transports["999"].GetNumberOfPassengers();
+
+                Console.WriteLine($"Current number of passengers: {num}");
+
+                foreach (var p in Transports["999"].GetCurrentPassengers())
+                {
+                    p.PrintPassengerDetails();
+                }
             }
             catch (Exception ex)
             {
